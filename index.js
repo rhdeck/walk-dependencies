455a4130f2d7a291b483911ee4f2523147a7c763
Ray Deck <ray@raydeck.com>
2018-01-18 10:22:44 -0500

    Adding support for returning "Ancestors" as array so you know how deep you are in the tree on each callback

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const fs = require("fs");
 const Path = require("path");
-function walkDependencies(path, useDevDependencies, cb) {
+function walkDependencies(path, useDevDependencies, cb, ancestors) {
   if (!path) return null;
   const p = readPackageFromPath(path);
+  if (!ancestors) ancestors = [];
+  ancestors.push({ name: p.name, version: p.version });
   if (p) {
     Object.keys(returnif(p.dependencies)).forEach(key => {
-      walkDependencies(resolve(key), useDevDependencies, cb);
+      walkDependencies(resolve(key), useDevDependencies, cb, ancestors);
     });
     if (useDevDependencies) {
       Object.keys(returnif(p.devDependencies)).forEach(key => {
-        walkDependencies(resolve(key), useDevDependencies, cb);
+        walkDependencies(resolve(key), useDevDependencies, cb, ancestors);
       });
     }
-    cb(path, p);
+    cb(path, p, ancestors);
   }
 }
 function returnif(obj) {
